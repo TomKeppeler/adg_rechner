95e473f36a46c1a4c6863375e16781f3f05c534d
Tom Keppeler <49951477+TomKeppeler@users.noreply.github.com>
2021-12-08 16:33:23 +0100

    Update ActivityMain.java

--- src/ActivityMain.java
+++ src/ActivityMain.java
@@ -3,7 +3,6 @@ import alg.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
 
 /**
  * @author Tom Keppeler tomkeppeler25gmail.com
@@ -20,7 +19,6 @@ public class ActivityMain {
     private JButton mergeSortButton;
     private JButton heapSortButton;
     private JRadioButton zahlenRadioButton;
-    private JRadioButton textRadioButton;
 
     ActivityMain() {
         selectionSortButton.addActionListener(new ActionListener() {
